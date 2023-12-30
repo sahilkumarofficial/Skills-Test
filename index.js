b1de8b06044065bb0bd97dde5aea156bcf366fa6
sahil kumar <146019508+sahilkumarofficial@users.noreply.github.com>
2023-12-30 13:28:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+function openhome() {
+    window.location.href = 'index.html';
+}
+function openalltest() {
+    window.location.href = 'alltest.html';
+}
+
+function openupcoming() {
+    window.location.href = 'admin-pannel.html';
+}
+function openresults() {
+    window.location.href = 'Result.html';
+}
+function openbuytest() {
+    window.location.href = 'buy.html';
+}
+function login() {
+    window.location.href = 'login.html';
+}
+function create() {
+      window.location.href = 'create.html';
+  }
+  function forgot() {
+      window.location.href = 'forgot.html';
+  }
+  function start() {
+    window.location.href = 'lesson.html';
+}
+function join() {
+    window.location.href = 'Question.html';
+}
+function submit() {
+    window.location.href = 'Result.html';
+}
+function viewanswer() {
+    window.location.href = 'view.html';
+}
+function saveresult() {
+    window.location.href = 'index.html';
+}
